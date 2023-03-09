ccac5cf8f000c27f53aed647c700da24504c3dd4
Andrew Peterson <andrewpetersondev@gmail.com>
2023-03-09 14:53:06 -0600

    reverted commit

--- models/index.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    User: require('./user'),
-    Category: require('./category'),
-    Goal: require('./goal'),
-    Task: require('./task'),
-    UserGoal: require('./userGoal'),
-    TaskTimeline: require('./taskTimeline')
-}
