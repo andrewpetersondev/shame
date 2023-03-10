44b82a7d7374f1ea2278efe61fb2184e548f211b
Andrew Peterson <andrewpetersondev@gmail.com>
2023-03-10 10:53:40 -0600

    removed unused variable

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 // import components that appear on every page
 import Navbar from "./components/Navbar";
