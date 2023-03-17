63891e517a53f13c0f081e2a5574cd06e09cc676
Andrew Peterson <andrewpetersondev@gmail.com>
2023-03-17 14:44:18 -0500

    converted to functional component

--- client/src/pages/Manage/index.js
+++ client/src/pages/Manage/index.js
@@ -1,31 +1,29 @@
-// react
-import React from "react";
+import React, { useEffect } from "react";
 import "./style.css";
 
 // react bootstrap
-import Container from 'react-bootstrap/Container'
-import Row from 'react-bootstrap/Row'
-import Col from 'react-bootstrap/Col'
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
-// components
-// import Container from "../../components/Container";
-// import { Col, Row } from "../../components/Grid";
+// components
 import UserGoalCard from "../../components/UserGoalCard";
-// import GoalCard from '../../components/GoalCard';
 
-class Manage extends React.Component {
+function Manage(props) {
+    useEffect(() => {
+        // componentDidMount logic here
+    }, []);
 
-    componentDidMount = () => { }
-
-    render() {
-        return (
-            <Container>
-                <Row className="userGoalRow">
-                    <Col size="md-12 col-auto" className="userGoalCol">
-                        <h2 className="manageH2">Active Goals</h2>
-                        <div className="userGoalDiv">
-                            {this.props.userGoals ? (
-                                this.props.userGoals.filter(userGoal => userGoal.goalPercent !== 100).map(userGoal => (
+    return (
+        <Container>
+            <Row className="userGoalRow">
+                <Col size="md-12 col-auto" className="userGoalCol">
+                    <h2 className="manageH2">Active Goals</h2>
+                    <div className="userGoalDiv">
+                        {props.userGoals ? (
+                            props.userGoals
+                                .filter((userGoal) => userGoal.goalPercent !== 100)
+                                .map((userGoal) => (
                                     <UserGoalCard
                                         id={userGoal.userGoalId}
                                         key={userGoal.userGoalId}
@@ -34,37 +32,35 @@ class Manage extends React.Component {
                                         goalPercent={userGoal.goalPercent}
                                     />
                                 ))
-                            ) : (
-                                <span>No goals in this section.</span>
-                            )}
-                        </div>
-                    </Col>
-                </Row>
-                <Row className="userGoalRow" size="md-12">
-                    <Col size="md-12 col-auto" className="userGoalCol">
-                        <h2 className="manageH2">Achieved Goals</h2>
-                        <div className="userGoalDiv">
-                            {this.props.userGoals ? (
-                                this.props.userGoals.filter(userGoal => userGoal.goalPercent === 100).map(userGoal =>
-                                (
+                        ) : (
+                            <span>No goals in this section.</span>
+                        )}
+                    </div>
+                </Col>
+            </Row>
+            <Row className="userGoalRow" size="md-12">
+                <Col size="md-12 col-auto" className="userGoalCol">
+                    <h2 className="manageH2">Achieved Goals</h2>
+                    <div className="userGoalDiv">
+                        {props.userGoals ? (
+                            props.userGoals
+                                .filter((userGoal) => userGoal.goalPercent === 100)
+                                .map((userGoal) => (
                                     <UserGoalCard
                                         id={userGoal.userGoalId}
                                         key={userGoal.userGoalId}
                                         goalName={userGoal.goalName}
                                         goalPercent={userGoal.goalPercent}
                                     />
-                                )
-
-                                )
-                            ) : (
-                                <span>No goals in this section.</span>
-                            )}
-                        </div>
-                    </Col>
-                </Row>
-            </Container>
-        );
-    }
+                                ))
+                        ) : (
+                            <span>No goals in this section.</span>
+                        )}
+                    </div>
+                </Col>
+            </Row>
+        </Container>
+    );
 }
 
 export default Manage;
